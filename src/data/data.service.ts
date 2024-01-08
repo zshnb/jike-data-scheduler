1ce018670d2376c3657043ee9c5eb4b681349fd5
zshnb <a857681664@gmail.com>
2024-01-08 19:01:12 +0800

    adjust cron time

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
-import { Cron } from '@nestjs/schedule'
 import { PrismaService } from '../prisma.service'
 import { JikeClient } from '../jikeClient'
+import { Cron } from '@nestjs/schedule'
 
 @Injectable()
 export class DataService {
@@ -10,11 +10,14 @@ export class DataService {
     private jikeClient: JikeClient,
   ) {}
 
+  @Cron('0 0 * * *')
   async fetchFollowerCount() {
+    console.log('start fetch followerCount')
     const users = await this.prisma.user.findMany()
     for (const user of users) {
       let refreshToken = user.jikeRefreshToken
       let accessToken = user.jikeAccessToken
+      let userId = user.id
       try {
         const profile = await this.jikeClient.getProfile({
           accessToken,
@@ -24,6 +27,19 @@ export class DataService {
           accessToken = profile.accessToken
           refreshToken = profile.refreshToken
         }
+        const mayExistUser = await this.prisma.user.findFirst({
+          where: {
+            username: profile.user.screenName,
+          },
+        })
+        if (mayExistUser !== null) {
+          userId = mayExistUser.id
+          this.prisma.user.delete({
+            where: {
+              id: user.id,
+            },
+          })
+        }
         await this.prisma.user.update({
           data: {
             username: profile.user.screenName,
@@ -31,13 +47,13 @@ export class DataService {
             jikeRefreshToken: refreshToken,
           },
           where: {
-            id: user.id,
+            id: userId,
           },
         })
         await this.prisma.data.create({
           data: {
             followerCount: profile.user.statsCount.followedCount,
-            userId: user.id,
+            userId: userId,
             date: new Date().getTime(),
           },
         })
